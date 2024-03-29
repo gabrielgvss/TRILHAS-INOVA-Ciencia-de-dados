9aef03b9674784009150d9c6ec0480a6273c737a
gabrielgvss <gabriel.gvss@gmail.com>
2024-03-29 11:52:57 -0300

    adicao de contagem de numeros ja gerados

--- logica_de_programacao/logica-js-projeto02/js-curso-2-aula_-/app.js
+++ logica_de_programacao/logica-js-projeto02/js-curso-2-aula_-/app.js
@@ -1,11 +1,27 @@
-function gerarNumeroSecreto(numero_minimo, numero_maximo) {
-    return parseInt(Math.random() * numero_maximo + numero_minimo);
-
-}
-
 function mudarTextoElemento(elemento_html, texto){
     let elemento = document.querySelector(elemento_html);
     elemento.innerHTML = texto;
+
+}
+
+function exibirMensagemInicial(){
+    mudarTextoElemento('h1', 'Jogo do número secreto');
+    mudarTextoElemento('p', 'Escolha um número de 1 a 10:');
+
+}
+
+function gerarNumeroSecreto(numero_minimo, numero_maximo) {
+    let num_secreto;
+
+    do {
+        num_secreto = parseInt(Math.random() * numero_maximo + numero_minimo);
+
+    } while (numeros_gerados.includes(num_secreto));
+
+    numeros_gerados.push(num_secreto);
+    console.log("Número secreto da rodada: "+num_secreto);
+
+    return num_secreto;
 
 }
 
@@ -14,9 +30,7 @@ function limparInput(){
 }
 
 function verificarChute(){
-    let chute = document.querySelector('input');
-
-    chute = parseInt(chute.value);
+    let chute = parseInt(document.querySelector('input').value);
 
     if (chute == numero_secreto){
         mudarTextoElemento('h1', "Acertou!");
@@ -32,27 +46,43 @@ function verificarChute(){
         document.getElementById('reiniciar').disabled = false;
         document.querySelector('input').readOnly = true;
 
+    }else if (isNaN(chute)){
+        mudarTextoElemento('p', `Insira um valor para poder fazer o chute!`);
+
     }else{
         let texto = (chute < numero_secreto ? `Ainda não acertou :(, O número secreto é maior que ${chute}`:`Ainda não acertou :(, O número secreto é menor que ${chute}`);
         mudarTextoElemento('p', texto);
         tentativa++;
         limparInput();
-
     }
 
 }
 
 function reiniciar(){
-    location.reload(true);
+    limparInput();
+    tentativa = 1;
+    numero_secreto = gerarNumeroSecreto(1,10);
+    document.getElementById('btn_chute').disabled = false;
+    document.getElementById('reiniciar').disabled = true;
+    document.querySelector('input').readOnly = false;
+    exibirMensagemInicial();
+    mostrarNumerosGerados();
 
 }
 
-let chute = parseInt(document.querySelector('input').value);
+function mostrarNumerosGerados(){
+    for (let i = 0; i<numeros_gerados.length; i++){
+        console.log("Números gerados: "+ numeros_gerados[i] + "\t");
+
+    }
+}
+
+exibirMensagemInicial();
+let numeros_gerados = [];
 let numero_secreto = gerarNumeroSecreto(1,10);
 let tentativa = 1;
-console.log(numero_secreto);
 
-mudarTextoElemento('h1', "Jogo do número secreto");
-mudarTextoElemento('p', "Escolha um número de 1 a 10:");
+
+
 
 
