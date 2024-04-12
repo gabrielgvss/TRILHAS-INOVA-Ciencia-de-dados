12deca2b937d04545413a3186549f72fd0105ff8
Gabriel <gabriel.gvss@gmail.com>
2024-04-12 18:03:22 -0300

    desafio 02 - parcialmente concluído

---
+++ logica_de_programacao/desafio02/questao02/01.js
@@ -0,0 +1,12 @@
+function operacao(num1, num2){
+    let soma = num1 + num2;
+    let subtracao = num1 - num2;
+    let resto = num1 % num2;
+    let exponenciacao = Math.pow(num1, num2);
+
+    console.log(soma + subtracao + resto + exponenciacao);
+    
+}
+
+operacao(2,2);
+
---
+++ logica_de_programacao/desafio02/questao02/02.js
@@ -0,0 +1,32 @@
+const taxa_juros = (0.6022/100)
+let [valor_aplicado, tempo_aplicacao] = solicitar_dados();
+calcularRendimento(valor_aplicado, tempo_aplicacao);
+
+
+function solicitar_dados(){
+    let valor_aplicado = parseFloat(prompt('Digite o valor aplicado: '));
+    let tempo_aplicacao = parseInt(prompt('Digite a quantidade de meses em aplicação: '));
+    
+    while (isNaN(valor_aplicado) || isNaN(tempo_aplicacao) || valor_aplicado < 0 || tempo_aplicacao < 0){
+        alert('Valor inválido inserido em um dos campos! Por favor tente novamente:');
+        valor_aplicado = parseFloat(prompt('Digite o valor aplicado: '));
+        tempo_aplicacao = parseInt(prompt('Digite a quantidade de meses em aplicação: '));
+
+     }
+
+    
+    return [valor_aplicado, tempo_aplicacao];
+}
+
+
+
+function calcularRendimento(capital_aplicado, tempo){
+    let rendimento = capital_aplicado * (Math.pow((1 + taxa_juros), tempo)) - capital_aplicado;
+
+    alert(
+        `A quantia de
+        R$${capital_aplicado} aplicada em ${tempo} meses, rendeu R$${rendimento}
+        a juros de ${taxa_juros*100}% ao mês`
+    );
+}
+ 
